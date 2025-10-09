03efb7f81acf8c4c155ca7f450c3dc223a2a9995
Joe Tisseman <Joetisseman@gmail.com>
2025-10-09 12:20:59 +0100

    Potential variables

---
+++ Try1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+agent_position = np.
+agent_direction = np.
+v = np. #Distance travelled by each agent during time taken to update
+range = #Range of interaction
+nearest_neighbours = np. #Number of neighbours each agent interacts with
+plane = #Size of plane agents move on
+noise = 
+vector_motion = #velocity of each agent
+
