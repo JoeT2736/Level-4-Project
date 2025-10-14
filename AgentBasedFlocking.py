d012d9e362cffd2f290633a7a21392861112fc11
Joe Tisseman <Joetisseman@gmail.com>
2025-10-14 19:28:30 +0100

    1

--- AgentBasedFlocking.py
+++ AgentBasedFlocking.py
@@ -1,16 +1,24 @@
 import numpy as np
 
+time_update = 0.5
+distance = np.ones(10)
+neighbours = 4
+
+
 def velocity_update(distance, noise, neighbours, weight, force, time_update, velocity_neighbour):
 
     def neighbour_weight(neighbours, weight, velocity_neighbour):
+        j = np.zeros(np.len(neighbours))
         for i in range(neighbours):
-            j = weight[i] * velocity_neighbour[i]
+            j[i] = weight[i] * velocity_neighbour[i]
         return j
     
-    def attraction_force(force, distance, noise, neighbours):
+    def attraction_force(force, distance, neighbours):
+        k = np.zeros(np.len(neighbours))
         for i in range(neighbours):
-            k = (force[i] * (distance[i]/np.mod(distance[i])) + noise[i])
+            k[i] = force[i] * (distance[i]/np.mod(distance[i]))
         return k
     
-    return (1/neighbours) * (neighbour_weight(neighbours, weight, velocity_neighbour) + attraction_force(force, distance, noise, neighbours))        
-4
+    return (1/neighbours) * (neighbour_weight(neighbours, weight, velocity_neighbour) 
+                            + attraction_force(force, distance, noise, neighbours)) + noise  
+
