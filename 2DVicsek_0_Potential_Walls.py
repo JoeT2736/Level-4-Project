eb13d0f1315c08698c9ae4b4b78bcafdef52f6d3
Joe Tisseman <Joetisseman@gmail.com>
2025-10-29 21:37:36 +0000

    Quivers working, not all agents point in the direction of motion though

--- 2DVicsek_0_Potential_Walls.py
+++ 2DVicsek_0_Potential_Walls.py
@@ -23,19 +23,17 @@ import scipy.spatial
 
 #fig = plt.figure(figsize=(8, 8))
 
-N=300  #Number of agents
-D=5  #Size of domain
+N=200  #Number of agents
+D=10  #Size of domain
 T=600   #Total number of time steps (frames) in simulation
 stepsize=1  #change in time between calculation of position and angle
 eta=0.1   #Random noise added to angles
-v0=0.00003    #Starting velocity
+v0=0.001   #Starting velocity
 R=1    #Interaction radius
 
 pos = np.random.uniform(0, D, size=(N, 2))
-angle = np.random.uniform(-np.pi, np.pi, size=N)
+angle = np.random.uniform(0, 2*np.pi, size=N)
 
-#print(pos)
-#print(pos.reshape(2, N))
 
 def Vicsek():
 
@@ -44,75 +42,63 @@ def Vicsek():
 
     MeanAngle = np.zeros(N, )
 
-    noise = np.random.uniform(-eta/2, eta/2, size=(N))
-
     for i in range(N):
+
+        noise = np.random.uniform(-eta/2, eta/2, size=(N))
 
         DistanceMatrix = scipy.spatial.distance.pdist(pos)  #Scipy function to calculate distance between two agents
         DistanceMatrix = scipy.spatial.distance.squareform(DistanceMatrix)   #matrix of form [i, j] => distance between agents i and j
-        Neighbours = DistanceMatrix <= R #Gives array of True/False
+        #returns the distance of each agent to all other agents, the array is of size [N, N]
 
-        MeanAngle[i] = (np.arctan2(np.sum(np.sin(angle[Neighbours[:, i]])), np.sum(np.cos(angle[Neighbours[:, i]])))) #+ noise[Neighbours[:, i]]
+        Neighbours = DistanceMatrix <= R #Gives array of True/False, if distance less than R, this returns True
 
-        cos = (np.cos(MeanAngle + noise))
-        sin = (np.sin(MeanAngle + noise))
+        MeanAngle[i] = (np.arctan2(np.sum(np.sin(angle[Neighbours[:, i]])), np.sum(np.cos(angle[Neighbours[:, i]]))))
+                                                        #^^^^^Angles of the agents within R, the True values in 'Neighbours'
+        #Equation as in Vicsek 1995 to get the average angle of all the neighbours
 
-        g = np.array([cos, sin])
+        MeanAngle = MeanAngle + noise   #Adding random noise element to the angle
+        MeanAngle = np.mod(MeanAngle, 2*np.pi)
 
-        k = np.transpose(g)
+        #x and y directions accoring to new angle
+        cos = (np.cos(MeanAngle))   
+        sin = (np.sin(MeanAngle))
 
-        vel = v0 * k
-
-        pos = pos + vel * stepsize
+        #Updating the position of the agents
+        pos[:, 0] += cos * v0 * stepsize
+        pos[:, 1] += sin * v0 * stepsize
 
     pos = np.mod(pos, D)    #Agent appears on other side of domain when goes off one end
 
-    #angle = MeanAngle + noise
-
-    #noise = np.random.uniform(-eta/2, eta/2, size=N)
-    #angle = np.mod(angle + MeanAngle + noise, 2*np.pi)
-    return pos, k#, MeanAngle
-
-#print(Vicsek()[1][:, 0])
-
-#animated_plot = plt.quiver([], [], [], [], [])
-
-#plt.xlim(0, D)
-#plt.ylim(0, D)
+    return pos, cos, sin, MeanAngle
 
 fig, ax = plt.subplots()
 ax.set_xlim([0, D])
 ax.set_ylim([0, D])
-animated_plot, = ax.plot([], [], '.')
-#animated_plot_quiver, = ax.quiver([], [], [], [])
+animated_plot_quiver = ax.quiver(pos[:, 0], pos[:, 1], np.cos(angle), np.sin(angle), angle)
 
 
-plt.rcParams.update({'font.family': 'Times New Roman', 'font.size': 12})
+plt.rcParams["font.family"] = "Times New Roman"
 plt.title(f'Noise level = {eta}', fontsize=16)
 plt.xlabel('X-axis', fontsize=14)
 plt.ylabel('Y-axis', fontsize=14)
 plt.setp(ax.get_xticklabels(), visible='False')
 plt.setp(ax.get_yticklabels(), visible='False')
+plt.locator_params(axis='y', nbins=1)
+plt.locator_params(axis='x', nbins=1)
 ax.set_aspect('equal', adjustable='box')
 
 
-#def Animate_quiver(frame):
-#    animated_plot_quiver.set_data()
-#    return
+def Animate_quiver(frame):
+    animated_plot_quiver.set_offsets(Vicsek()[0])
+    animated_plot_quiver.set_UVC(Vicsek()[1], Vicsek()[2], Vicsek()[3])
+    return (animated_plot_quiver, )
 
+anim = FuncAnimation(fig = fig, func = Animate_quiver, interval = 1, frames = T, blit = False, repeat=False)
 
-def Animation(frame):
-    animated_plot.set_data([Vicsek()[0][:, 0], Vicsek()[0][:, 1]])
-    return 
-
-anim = FuncAnimation(fig = fig, func = Animation, interval = 1,frames = T, blit = False)
-
-anim.save("2DVicsekAnimation.gif", dpi=400)
-plt.savefig("2DVicsekAnimation.png", dpi=400)
+#anim.save("2DVicsekAnimation.gif", dpi=400)
+#plt.savefig("2DVicsekAnimation.png", dpi=400)
 plt.show()
 
-
-        
 
 
 
