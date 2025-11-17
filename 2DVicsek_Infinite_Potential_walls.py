4298bfabeafe22fa2c82329f16add235c21d3246
Joe Tisseman <Joetisseman@gmail.com>
2025-11-17 13:18:00 +0000

    Get stuck in corners, bottom wall only forces one way???, forces now in own function

--- 2DVicsek_Infinite_Potential_walls.py
+++ 2DVicsek_Infinite_Potential_walls.py
@@ -7,37 +7,42 @@ import scipy.constants
 
 N=30  #Number of agents
 D=5  #Size of domain
-T=600   #Total number of time steps (frames) in simulation
+T=6000   #Total number of time steps (frames) in simulation
 stepsize=1  #change in time between calculation of position and angle
 eta=0.15   #Random noise added to angles
 v0=0.03   #Starting velocity
 R=1    #Interaction radius
 scale = 2
 force_scale = 0.005
+wall = 1
 
-pos = np.random.uniform(0+0.5, D-0.5, size=(N, 2))
+
+pos = np.random.uniform(0, D, size=(N, 2))
 angle = np.random.uniform(0, 2*np.pi, size=N)
 
+def wall_force(position, wall, direction, choice):
+    dist = wall - position
+    if dist < wall:      #if close enough then...
+        force_wall = 1/(dist)**scale      #potential due to wall
+        if direction >= choice:      #choice = angle where boid turns either up/down or left/right based on its direction
+            force_wall = force_wall
+        elif direction < choice:
+            force_wall = -force_wall
+    else:
+        force_wall = 0
+
+    return force_wall
 
 def Vicsek():
     global pos
     global angle
 
-    MeanAngle = np.zeros(N, )
+    Meandirection = np.zeros(N, )
     noise = np.random.uniform(-eta/2, eta/2, size=(N))
-    #cos = np.zeros(N)
-    #sin = np.zeros(N)
-    #vx = np.zeros(N)
-    #vy = np.zeros(N)
-    force_wall = np.zeros(N)
     force_wall_left = np.zeros(N)
     force_wall_right = np.zeros(N)
     force_wall_bot = np.zeros(N)
     force_wall_top = np.zeros(N)
-    dx_left = np.zeros(N)
-    dx_right = np.zeros(N)
-    dy_bot = np.zeros(N)
-    dy_top = np.zeros(N)
 
     DistanceMatrix = scipy.spatial.distance.pdist(pos)  #Scipy function to calculate distance between two agents
     DistanceMatrix = scipy.spatial.distance.squareform(DistanceMatrix)   #matrix of form [i, j] => distance between agents i and j
@@ -47,47 +52,24 @@ def Vicsek():
 
         Neighbours = DistanceMatrix <= R #Gives array of True/False, if distance less than R, this returns True
         
-        MeanAngle[i] = (np.arctan2(np.mean(np.sin(angle[Neighbours[:, i]])), np.mean(np.cos(angle[Neighbours[:, i]])))) + noise[i]
+        Meandirection[i] = (np.arctan2(np.mean(np.sin(angle[Neighbours[:, i]])), np.mean(np.cos(angle[Neighbours[:, i]])))) + noise[i]
                                                         #^^^^^Angles of the agents within R, the True values in 'Neighbours'
         #Equation as in Vicsek 1995 to get the average angle of all the neighbours
 
+        #### Instead of closest distance to wall to find force, 
+        # force should be based off of distance to wall in the LOS of the boid ####
 
-        dx_left[i] = pos[i, 0]
-        dx_right[i] = D - pos[i, 0]
-        dy_bot[i] = pos[i, 1]
-        dy_top[i] = D - pos[i, 1]
+        force_wall_left[i] = wall_force(pos[i, 0], 0, Meandirection[i], np.pi)
+        force_wall_right[i] = wall_force(pos[i, 0], D, Meandirection[i], 0)
+        force_wall_bot[i] = wall_force(pos[i, 1], 0, Meandirection[i], 3*np.pi/2)
+        force_wall_top[i] = wall_force(pos[i, 1], D, Meandirection[i], np.pi/2)
 
-        force_wall_left[i] = 1/(dx_left[i])**scale
-        force_wall_right[i] = 1/(dx_right[i])**scale
-        force_wall_bot[i] = 1/(dy_bot[i])**scale    
-        force_wall_top[i] = 1/(dy_top[i])**scale
+    force_wall = force_wall_left + force_wall_right + force_wall_bot + force_wall_top
 
-        if MeanAngle[i] >= 0:
-            force_wall_left[i] = -force_wall_left[i]
-        elif MeanAngle[i] < 0:
-            force_wall_left[i] = force_wall_left[i]
+    Meandirection = Meandirection + force_wall*force_scale
 
-        if MeanAngle[i] >= 0:
-            force_wall_right[i] = force_wall_right[i]
-        elif MeanAngle[i] < 0:
-            force_wall_right[i] = -force_wall_right[i]
-
-        if MeanAngle[i] >= 3*np.pi/2:
-            force_wall_bot[i] = force_wall_bot[i]
-        elif MeanAngle[i] < 3*np.pi/2:
-            force_wall_bot[i] = -force_wall_bot[i]
-
-        if MeanAngle[i] >= np.pi/2:
-            force_wall_top[i] = force_wall_top[i]
-        elif MeanAngle[i] < np.pi/2:
-            force_wall_top[i] = -force_wall_top[i]
-
-        force_wall[i] = force_wall_left[i] + force_wall_right[i] + force_wall_bot[i] + force_wall_top[i]
-
-    MeanAngle = MeanAngle + force_wall*force_scale
-
-    cos = np.cos(MeanAngle)
-    sin = np.sin(MeanAngle)
+    cos = np.cos(Meandirection)
+    sin = np.sin(Meandirection)
 
     vx = cos * v0 * stepsize
     vy = sin * v0 * stepsize
@@ -96,9 +78,9 @@ def Vicsek():
     pos[:, 0] += vx
     pos[:, 1] += vy
 
-    angle = MeanAngle
+    angle = Meandirection
 
-    return pos, cos, sin, MeanAngle, vx
+    return pos, cos, sin
 
 
 
@@ -120,7 +102,7 @@ ax.set_aspect('equal', adjustable='box')
 
 
 def Animate_quiver(frame):
-    pos, cos, sin, MeanAngle, vx = Vicsek()
+    pos, cos, sin = Vicsek()
     animated_plot_quiver.set_offsets(pos)
     animated_plot_quiver.set_UVC(cos, sin)
     return (animated_plot_quiver,)
@@ -128,39 +110,8 @@ def Animate_quiver(frame):
 
 anim = FuncAnimation(fig = fig, func = Animate_quiver, interval = 1, frames = T, blit = False, repeat=False)
 
-anim.save(f"Potential Wall, Noise Level = {eta}, N={N}, D={D}, potential scale = {force_scale}.gif", dpi=400)
+#anim.save(f"Hemelrijk, Noise Level = {eta}, N={N}, D={D}, potential scale = {force_scale}.gif", dpi=400)
 #plt.savefig("2DVicsekAnimation.png", dpi=400)
 plt.show()
 
 
-
-
-
-
-
-
-        ### Potential Walls ###
-'''
-        scale = 2
-        force_wall[i] = 1/(dx_left[i])**scale + 1/(dx_right[i])**scale + 1/(dy_bot[i])**scale + 1/(dy_top[i])**scale
-        #forces should be seperated to x and y directions ???
-
-        if dx_left[i] < 1:
-            fwx_left[i] = 1/(dx_left[i])**scale
-        if dx_right[i] < 1:
-            fwx_right[i] = 1/(dx_right[i]**scale)
-
-        if dy_top[i] < 1:
-            fwy_top[i] = 1/(dy_top[i])**scale
-        if dy_bot[i] < 1:
-            fwy_bot[i] = 1/(dy_bot[i]**scale)
-
-        force_wall_x[i] = fwx_left[i] + fwx_right[i]
-        force_wall_y[i] = fwy_bot[i] + fwy_top[i]
-        
-    #Seperate x and y forces converted to angle
-    wall_angle = np.arctan2(np.sin(force_wall_y), np.cos(force_wall_x))
-    #Angle goes to pi then stops, and stays at pi     
-    
-    #MeanAngle += force_wall    #agents walk around close to edge of domain
-    '''
