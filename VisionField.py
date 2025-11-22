4364f47e872cf40a88909315c84271f2c1578749
Joe Tisseman <Joetisseman@gmail.com>
2025-11-22 12:23:36 +0000

    updated new wall force function

--- VisionField.py
+++ VisionField.py
@@ -5,60 +5,53 @@ import scipy.spatial
 import scipy.constants
 
 
-N=10  #Number of agents
+N=30  #Number of agents
 D=5  #Size of domain
 T=6000   #Total number of time steps (frames) in simulation
 stepsize=1  #change in time between calculation of position and angle
 eta=0.15   #Random noise added to angles
 v0=0.03   #Starting velocity
 R=1    #Interaction radius
-scale = 2
-force_scale = 0.005
+scale = 3
+force_scale = 0.01
 w = np.pi/2   #FOV
 fake_wall = 1
+epsilon = 1e-6
+
+np.random.seed(10)
 
 pos = np.random.uniform(0, D, size=(N, 2))
 angle = np.random.uniform(0, 2*np.pi, size=N)
 
-def wall_force(position, wall, direction, choice):
-    dist = wall - position
-    '''
-    if direction <= np.pi/2 and direction >= 0:
-        dist = (wall - position) * np.cos(direction)
-    
-    if direction > np.pi/2 and direction <= np.pi:
-        dist = (wall - position) * np.sin(direction)
+def wall_force_vector(position, angle):
+                         # x and y direction of the boid
+    direction = np.array([np.cos(angle), np.sin(angle)])  
+    Force = np.zeros(2)     # setting array for force due to walls
 
-    if direction > np.pi and direction <= 3*np.pi/2:
-        dist = (wall - position) * np.cos(direction - np.pi)
-    
-    else:
-        dist = (wall - position) * np.sin(direction - np.pi)
-        '''
-    if abs(dist) < fake_wall:      #if close enough then...
-        force_wall = 1/(abs(dist))**scale      #potential due to wall
-        if direction >= choice:      #choice = angle where boid turns either up/down or left/right based on its direction
-            force_wall = force_wall
-        elif direction < choice:
-            force_wall = -force_wall
-    else:
-        force_wall = 0
+    # distance to walls
+    dist_left = position[0]         # x = 0
+    dist_right = D - position[0]    # x = D
+    dist_bot = position[1]          # y = 0
+    dist_top = D - position[1]      # y = D
 
-    return force_wall
+    # normal vectors pointing away from each wall, alongside the boids respective distance to that wall
+    normals = [
+        (np.array([1.0, 0.0]), dist_left),       
+        (np.array([-1.0, 0.0]), dist_right),    
+        (np.array([0.0, 1.0]), dist_bot),        
+        (np.array([0.0, -1.0]), dist_top)        
+    ]
 
-R_circ = D/2
-centre = np.array([D/2, D/2])
-def wall_force_circle(position, centre, R_circ, scale):
-    vec = pos - centre
-    dist = np.linalg.norm(vec)
-    if dist >= R_circ:
-        return np.zeros(2)
+    # calculate force due to wall, and update 'Force', with the force and its direction using the normal vectors
+    for norm_vec, dist in normals:
+        if dist < fake_wall:
+            mag = 1.0 / ((dist + epsilon)**scale)   # epsilon for if distance = 0
+            Force += mag * norm_vec   
 
-    # outward normal
-    n = vec / dist
+    # changes the force from the wall, to a force that changes the direction of the boid, not the position
+    torque = direction[0]*Force[1] - direction[1]*Force[0]
 
-    strength = 1 / ((R_circ - dist)**scale)
-    return strength * n
+    return torque
 
 
 def Vicsek():
@@ -67,17 +60,13 @@ def Vicsek():
 
     Meandirection = np.zeros(N, )
     noise = np.random.uniform(-eta/2, eta/2, size=(N))
-    force_wall_left = np.zeros(N)
-    force_wall_right = np.zeros(N)
-    force_wall_bot = np.zeros(N)
-    force_wall_top = np.zeros(N)
+    total_wall_torque = np.zeros(N)
 
     DistanceMatrix = scipy.spatial.distance.pdist(pos)  #Scipy function to calculate distance between two agents
     DistanceMatrix = scipy.spatial.distance.squareform(DistanceMatrix)   #matrix of form [i, j] => distance between agents i and j
     #returns the distance of each agent to all other agents, the array is of size [N, N]
 
     for i in range(N):
-
 
         Distance_mask = DistanceMatrix <= R #Gives array of True/False, if distance less than R, this returns True
 
@@ -107,19 +96,10 @@ def Vicsek():
 
         #### Instead of closest distance to wall to find force, force should be based off of distance to wall in the LOS of the boid ####
 
-
-        force_wall_left[i] = wall_force(pos[i, 0], 0, Meandirection[i], np.pi)
-        #force_wall_left[i] = wall_force_circle(pos[i, 0], centre, R_circ, scale, )
-        force_wall_right[i] = wall_force(pos[i, 0], D, Meandirection[i], 0)
-        #force_wall_left[i] = wall_force_circle(pos[i, 0], centre, R_circ, scale)
-        force_wall_bot[i] = wall_force(pos[i, 1], 0, Meandirection[i], 3*np.pi/2)
-        #force_wall_left[i] = wall_force_circle(pos[i, 0], centre, R_circ, scale)
-        force_wall_top[i] = wall_force(pos[i, 1], D, Meandirection[i], np.pi/2)
-        #force_wall_left[i] = wall_force_circle(pos[i, 0], centre, R_circ, scale)
-
-    force_wall = force_wall_left + force_wall_right + force_wall_bot + force_wall_top
-
-    Meandirection = Meandirection + force_wall*force_scale
+        wall_torque = wall_force_vector(pos[i], Meandirection[i])
+        total_wall_torque[i] = wall_torque
+    
+    Meandirection = Meandirection + total_wall_torque * force_scale
 
     cos = np.cos(Meandirection)
     sin = np.sin(Meandirection)
@@ -133,10 +113,7 @@ def Vicsek():
 
     angle = Meandirection
 
-    return pos, cos, sin, vector, angle_difference
-
-print(Vicsek()[3])
-print(Vicsek()[4])
+    return pos, cos, sin
 
 
 fig, ax = plt.subplots()
@@ -157,11 +134,10 @@ ax.set_aspect('equal', adjustable='box')
 
 
 def Animate_quiver(frame):
-    pos, cos, sin, dots, t = Vicsek()
+    pos, cos, sin = Vicsek()
     animated_plot_quiver.set_offsets(pos)
     animated_plot_quiver.set_UVC(cos, sin)
     return (animated_plot_quiver,)
-#Animate_quiver
 
 anim = FuncAnimation(fig = fig, func = Animate_quiver, interval = 1, frames = T, blit = False, repeat=False)
 
